2a0e7a8f924e52a33bc4105c717557d2ebfc8d6e
Mekhla <sharma.mekhla1993@gmail.com>
2024-07-13 19:01:05 +0530

    Initial commit

---
+++ android/app/src/main/java/com/dementia/diagnostics/MainActivity.java
@@ -0,0 +1,5 @@
+package com.dementia.diagnostics;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dementia.diagnostics',
+  appName: 'Dementia Diagnosticsool',
+  webDir: 'www'
+};
+
+export default config;
+
+
