1ab4942382670902da7bc2003b522d673da26c1c
Mekhla <sharma.mekhla1993@gmail.com>
2024-07-18 02:00:34 +0530

    theme changes

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { UserData } from '../../providers/user-data';
 
 import { UserOptions } from '../../interfaces/user-options';
+import { AnimationService } from '../../providers/animation.service';
 
 
 
@@ -19,28 +20,31 @@ export class LoginPage {
 
   constructor(
     public userData: UserData,
-    public router: Router
+    public router: Router,
+    private animationService: AnimationService
   ) { }
 
-  onLogin(form: NgForm) {
-    this.submitted = true;
+  // onLogin(form: NgForm) {
+  //   this.submitted = true;
 
-    if (form.valid) {
-      this.userData.login(this.login.username);
-       this.router.navigateByUrl('/landing');
-     // this.router.navigateByUrl('/app/tabs/welcome');
+  //   if (form.valid) {
+  //     this.userData.login(this.login.username);
+  //     // this.router.navigateByUrl(`/app/tabs/landing/${this.login.username}`);
+  //     this.router.navigateByUrl(`/app/tabs/welcome`);
+  //     //this.router.navigateByUrl(`/landing`);
 
-      // this.router.navigateByUrl(`/app/tabs/landing/${this.login.username}`);
-     // this.router.navigateByUrl(`/app/tabs/welcome`);
-      //this.router.navigateByUrl(`/landing`);
-
-      //this.router.navigateByUrl(`/app/tabs/welcome`);
-    }
-  }
+  //     //this.router.navigateByUrl(`/app/tabs/welcome`);
+  //   }
+  // }
 
   openWelcomePage() {
     this.router.navigateByUrl(`/app/tabs/welcome`);
   }
+
+  // openWelcomePage(event: Event) {
+  //   const buttonElement = event.target as HTMLElement;
+  //   this.animationService.animateAndNavigate(buttonElement, '/app/tabs/welcome');
+  // }
 
   onSignup() {
     this.router.navigateByUrl('/signup');
