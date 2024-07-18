0e34d4cd1439ce7bfc878aa837f1feb839d873f5
UmeshK <Yashashri>
2024-07-18 18:59:52 +0530

    Changes for Feeds: Done

--- src/app/pages/map/map.ts
+++ src/app/pages/map/map.ts
@@ -59,62 +59,9 @@ export class MapPage implements AfterViewInit {
       });
 
     });
-    // this.confData.getMap().subscribe((mapData: any) => {
-    //   const mapEle = this.mapElement.nativeElement;
-    //   const coordinates = {
-    //     lat: this.coordinates.__zone_symbol__value.coords.latitude,
-    //     lng: this.coordinates.__zone_symbol__value.coords.longitude
-    //   };
-    //   map = new googleMaps.Map(mapEle, {
-    //     center: coordinates,
-    //     zoom: 16,
-    //     styles: style
-    //   });
-
-    //   mapData.forEach((markerData: any) => {
-    //     const infoWindow = new googleMaps.InfoWindow({
-    //       content: `<h5>${markerData.name}</h5>`
-    //     });
-
-    //     const marker = new googleMaps.Marker({
-    //       position: markerData,
-    //       map,
-    //       title: markerData.name
-    //     });
-
-    //     marker.addListener('click', () => {
-    //       infoWindow.open(map, marker);
-    //     });
-    //   });
-
-    //   googleMaps.event.addListenerOnce(map, 'idle', () => {
-    //     mapEle.classList.add('show-map');
-    //   });
-    // });
-
-    // const observer = new MutationObserver((mutations) => {
-    //   mutations.forEach((mutation) => {
-    //     if (mutation.attributeName === 'class') {
-    //       const el = mutation.target as HTMLElement;
-    //       isDark = el.classList.contains('ion-palette-dark');
-    //       if (map && isDark) {
-    //         map.setOptions({styles: darkStyle});
-    //       } else if (map) {
-    //         map.setOptions({styles: []});
-    //       }
-    //     }
-    //   });
-    // });
-    // observer.observe(appEl, {
-    //   attributes: true
-    // });
+   
   }
 }
-
-
-
-
-
 
 function getGoogleMaps(apiKey: string): Promise<any> {
   const win = window as any;
